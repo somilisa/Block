7fb32486bcd68bd2153034c31bec753fc9764897
"Lisa Okafor" <"somilisa1010@gmail.com">
2022-04-14 17:18:44 +0100

    my commit

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,11 +1,17 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.10;
 contract HelloWorld {
+    event UpdatedMessages(string oldStr,string newStr);
     string public message;
-    function setMessage(string memory _message) public {
+    constructor(string memory _message){
         message = _message;
     }
     function viewMessage() public view returns(string memory){
         return message;
+    }
+    function update(string memory newMsg) public {
+        string memory oldMsg = message;
+        message = newMsg;
+        emit UpdatedMessages(oldMsg,newMsg);
     }
 }
